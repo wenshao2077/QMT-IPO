32a675cb7ca0e257bf12f2885fad5a08eac567b0
wenshao2077 <ljc13035130955@gmail.com>
2026-09-14 10:52:34 +0800

    chore: identify delivery round 1 as 3.4.0-alpha1

---
+++ release_info.py
@@ -0,0 +1,6 @@
+"""Build identity for the first delivery iteration; not a production release."""
+VERSION = '3.4.0-alpha1'
+BASE_COMMIT = 'bd667bf8dd072e8467b91432f1eb9986bce43e15'
+DEPLOY_SCHEMA_VERSION = 1
+VERIFIED_PYTHON = (3, 11)
+PINNED_DEPENDENCIES = {'xtquant': '250807.1.2', 'numpy': '1.26.4', 'pandas': '2.2.3'}
